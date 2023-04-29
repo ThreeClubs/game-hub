51455ae223ee724cda03f735c28203608a7e8e15
Andrew Grenon <andrew.grenon@gmail.com>
2023-04-29 16:01:14 +1200

    implement colorMode switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  const [toggleLabel, setToggleLabel] = useState<string>("Dark");
+  
+  const handleToggleLabel = () => {
+    toggleColorMode()
+    setToggleLabel(toggleLabel === "Dark" ? "Light" : "Dark")
+  }
+
+  return (
+    <HStack>
+      <Switch colorScheme="green" isChecked={colorMode === 'dark'} onChange={handleToggleLabel}/>
+      <Text>{toggleLabel} Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
