92bd20e02830fbf96bb7eaecbdd7b55359a4eaa4
Andrew Grenon <andrew.grenon@gmail.com>
2023-04-30 13:31:14 +1200

    display platform icons

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -3,17 +3,11 @@ import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
-  const [toggleLabel, setToggleLabel] = useState<string>("Light");
-  
-  const handleToggleLabel = () => {
-    toggleColorMode()
-    setToggleLabel(toggleLabel === "Dark" ? "Light" : "Dark")
-  }
 
   return (
     <HStack>
-      <Switch colorScheme="green" isChecked={colorMode === 'dark'} onChange={handleToggleLabel}/>
-      <Text>{toggleLabel} Mode</Text>
+      <Switch colorScheme="green" isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
+      <Text>Dark Mode</Text>
     </HStack>
   );
 };
